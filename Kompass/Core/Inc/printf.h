0ee6bf0fddfc5417d65075bbb7a4a45059e298ba
Julian <julian.bauer@racing.tuwin.ac.at>
2025-12-06 09:32:30 +0100

    added Kompass projekt

---
+++ Kompass/Core/Inc/printf.h
@@ -0,0 +1,12 @@
+//
+// Created by VolkerTenta on 12/12/2023.
+//
+
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include "main.h"
+
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+
+#endif //PRINTF_H
